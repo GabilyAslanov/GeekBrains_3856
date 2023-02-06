fe5b350e5efb2b8b58e710fa522687d97ebbe650
GabilyAslanov <aslanov.gabily@gmail.com>
2023-02-06 21:03:39 +0300

    Seminar4

--- Seminar2/Program.cs
+++ Seminar2/Program.cs
@@ -17,3 +17,96 @@ else
 {
     Console.WriteLine($"number1 равно number2");
 }
+
+
+//Задача 10: Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
+
+Console.WriteLine("Enter number : ");
+int num = Convert.ToInt32(Console.ReadLine());
+
+// true && true = true    1 * 1 = 1
+// false && true = false 0 * 1 = 0
+// false && false = false 0 * 0 = 0
+// true || true = true  1 + 1 = 1
+// true || false = true  1 + 0 = 1
+// false || false = false 0 + 0 = 0
+if (num > 99 && num < 1000) 
+{
+    int secondNumber = (num / 10) % 10; //789/10= 78 %10=8
+
+    Console.WriteLine($"Вторая цифра в числе {num} -> {secondNumber}");
+}
+else
+    Console.WriteLine($"Число {num} не трёхзначное");
+
+
+
+
+
+
+//Задача 13: Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
+
+//645 -> 5
+
+// 78 -> третьей цифры нет
+
+// 32679 -> 6
+
+Console.WriteLine("Enter number : ");
+num = Convert.ToInt32(Console.ReadLine());
+
+// Console.WriteLine("Enter index : ");
+// int index = Convert.ToInt32(Console.ReadLine());
+
+//ouble pow = Math.Pow(10, index);
+
+//9
+if (num < 100)
+{
+    Console.WriteLine($"Цифра меньше индекса");
+  
+}
+else   {
+
+int tmp = num;
+
+while (tmp > 1000)// 987
+{
+    tmp /= 10;
+}
+
+int result = tmp % 10;
+
+Console.WriteLine($"Цифра под индексом 3 числа {num} -> {result}");
+}
+
+
+//Задача 15: Дано число обозначающее день недели. Выяснить является номер дня недели выходным
+Console.WriteLine("Enter dayWeek : ");
+string dayWeek = Console.ReadLine();
+
+switch (dayWeek)
+{
+    case "1":
+        Console.WriteLine("Понедельник- НЕТ");
+        break;
+    case "2":
+        Console.WriteLine("Вторник- НЕТ");
+        break;
+    case "3":
+        Console.WriteLine("Среда- НЕТ");
+        break;
+    case "4":
+        Console.WriteLine("Четверг- НЕТ");
+        break;
+    case "5":
+        Console.WriteLine("Пятница- Да");
+        break;
+    case "6":
+        Console.WriteLine("Суббота- Нет");
+        break;
+    case "7":
+        Console.WriteLine("Воскресенье- Нет");
+        break;
+  default : Console.WriteLine("Такого дня нет") ; break; 
+}
---
+++ Seminar4/Program.cs
@@ -0,0 +1,30 @@
+﻿// Задача 24: Напишите программу, которая принимает на вход число (А) и выдаёт сумму чисел от 1 до А.
+
+System.Console.Write("Enter number A :");
+int numA = int.Parse(Console.ReadLine());
+
+
+
+if (numA < 1)
+{
+    System.Console.WriteLine("Число меньше 1");
+}
+else
+{
+    int result = GetSum(numA);
+    System.Console.WriteLine($"sum =  {result}");
+    System.Console.WriteLine($"sum =  {GetSum(numA)}");
+}
+
+
+int GetSum(int number)
+{
+    int sum = 0;
+
+    for (int i = 1; i <= number; i++)
+    {
+        sum += i;
+    }
+
+    return sum;
+}
